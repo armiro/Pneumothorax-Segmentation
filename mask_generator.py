ff85dd15ca41a3434b468a40933bad7cf2c6abf4
armiro <arman.hgh.1374@gmail.com>
2020-11-16 16:00:22 -0600

    functionalize the code

--- mask_generator.py
+++ mask_generator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import operator
 import csv
 
@@ -20,28 +19,47 @@ def rle2mask(rle, width, height):
     return np.rot90(np.flip(mask.reshape(width, height), axis=1))
 
 
-md_path = './siim/train-rle.csv'
-image_shape = (1024, 1024)
-metadata = open(file=md_path, mode='r')
-records = csv.reader(metadata, delimiter=',')
-sorted_records = sorted(records, key=operator.itemgetter(0))[:-1]
+def generate_masks_from(metadata_path, mask_shape):
+    metadata = open(file=metadata_path, mode='r')
+    records = csv.reader(metadata, delimiter=',')
+    sorted_records = sorted(records, key=operator.itemgetter(0))[:-1]
 
-masks = list()
-for idx, rec in enumerate(sorted_records):
-    image_id = rec[0]
-    encoded_pixel = rec[1]
-    prev_image_id = sorted_records[idx-1][0]
+    masks = list()
+    for idx, rec in enumerate(sorted_records):
+        image_id = rec[0]
+        encoded_pixel = rec[1]
+        prev_image_id = sorted_records[idx - 1][0]
 
-    if encoded_pixel == '-1':
-        # create a black mask for cases with neg label
-        masks.append(np.zeros(shape=image_shape, dtype='uint8'))
-    else:
-        # convert RLE-coded data into mask images
-        mask = rle2mask(rle=encoded_pixel, width=image_shape[0], height=image_shape[1]).astype('uint8')
-        if image_id == prev_image_id:
-            last_mask = masks.pop()
-            masks.append(np.logical_or(mask, last_mask))
+        if encoded_pixel == '-1':
+            # create a black mask for cases with neg label
+            masks.append(np.zeros(shape=mask_shape, dtype='uint8'))
         else:
-            masks.append(mask)
+            # convert RLE-coded data into mask images
+            mask = rle2mask(rle=encoded_pixel, width=mask_shape[0], height=mask_shape[1]).astype('uint8')
+            if image_id == prev_image_id:
+                last_mask = masks.pop()
+                masks.append(np.logical_or(mask, last_mask))
+            else:
+                masks.append(mask)
+    return np.array(masks)
 
 
+def save_masks(masks):
+    print('number of images in the dataset with at least one corresponding mask:', len(masks))
+    print('shape of generated ground-truth:', masks.shape)
+    print('save the generated masks? (y/n) \n')
+    if input() == 'y':
+        np.save('./siim/training_masks.npy', arr=masks)
+        print('npy file saved!')
+    else:
+        print('saving masks ABORTED!')
+
+
+def main():
+    md_path = './siim/train-rle.csv'
+    generated_masks = generate_masks_from(metadata_path=md_path, mask_shape=(1024, 1024))
+    save_masks(masks=generated_masks)
+
+
+if __name__ == '__main__':
+    main()
